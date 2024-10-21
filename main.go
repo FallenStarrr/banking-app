7a3fc38e14e4648b0ed8ba0947baf16b27cc996f
FallenStarrr <juzopigeon@gmail.com>
2024-10-21 23:29:21 +0600

    db init

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/FallenStarrr/banking-app/config"
-import "fmt"
+import (
+	"github.com/FallenStarrr/banking-app/config"
+ "github.com/FallenStarrr/banking-app/repo"
+ "fmt"
+)
 
 func main() {
    c := config.GetConfig()
-	fmt.Println(c)
+	 fmt.Println(c)
+	 
+	db, err := repo.NewConn(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
 }
